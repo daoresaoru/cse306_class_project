3087aa37eb30404d81efefff973b1c5763f5f9c0
daoresaoru <149461277+daoresaoru@users.noreply.github.com>
2026-02-25 19:56:03 -0600

    Customer class

---
+++ customer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+class Customer {
+    private: 
+        int ID;
+        string first_name;
+        string last_name;
+        string address;
+        string city;
+        string state;
+        int zip;
+};
