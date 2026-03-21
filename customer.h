32711416df5eb24e5ba4f40e9688a1e9596ccca4
Ty-Tron <tytron2005@gmail.com>
2026-03-20 19:27:21 -0500

    Update customer.h
    Added accessor methods and the headers for mutator methods in case we need them later

--- customer.h
+++ customer.h
@@ -24,7 +24,51 @@ class Customer {
         string getCity();
         string getState();
         string getZIP();
-        void print();
-        
+
+        void setID(string newID);
+        void setFirstName(string newFirstName);
+        void setLastName(string newLastName);
+        void setAddress(string newAddress);
+        void setCity(string newCity);
+        void setState(string newState);
+        void setZIP(string newZIP);
+
+        Customer (string id, string first, string last, string a, string c, string s, string z){
+        	ID = id;
+        	first_name = first;
+        	last_name = last;
+        	address = a;
+        	city = c;
+        	state = s;
+        	zip = z;
+        }
 };
+
+string Customer::getID(){
+	return ID;
+}
+
+string Customer::getFirstName(){
+	return first_name;
+}
+
+string Customer::getLastName(){
+	return last_name;
+}
+
+string Customer::getAddress(){
+	return address;
+}
+
+string Customer::getCity(){
+	return city;
+}
+
+string Customer::getState(){
+	return state;
+}
+
+string Customer::getZIP(){
+	return zip;
+}
 #endif
