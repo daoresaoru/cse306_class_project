ac6e06855f07bcace6db4d5d0417c39d0bbc2cde
daoresaoru <149461277+daoresaoru@users.noreply.github.com>
2026-03-21 18:53:13 -0500

    Customer, Rainbow classes added main.cpp

--- main.cpp
+++ main.cpp
@@ -9,40 +9,77 @@
 #include <sstream>
 #include "transaction.h"
 #include "order.h"
+#include "customer.h"
+#include "rainbow.h"
 
 using namespace std;
+
 vector<Order> orders;
-map<int, Transaction> transactions;
-/*#include "customer.h"*/
+vector<Customer> customers;
+map<string, Transaction> transactions;
+vector<Rainbow> rainbows;
+
 
 
 int main() {
     ifstream infile("customers.txt", ios::in); 
     ifstream orders_file("orders.txt", ios::in);
-    ifstream transactions_file("orders.txt", ios::in);
-    ofstream outfile("results.txt", ios::out);
-    char option, sub_option;
-    string last_name, line_orders, line_transactions, line, word, date;
-    int cID, quantity, oID;
-    double price;
-    //Customer customer;
+    ifstream transactions_file("transactions.txt", ios::in);
+    ifstream rainbow_file("rainbowList.txt", ios::in); 
+    char option, sub_option; //these are for menu and Rainbow Tribble sub-menu
+    string last_name; //used for searching by last name 
+    string cID, oID; //customerID and orderID
+    string line_orders, line_transactions, line_customers, line_rainbow; //strings used for reading from files
+    string word, date; //other strings
+    string first, last, id, address, city, state, zip; //variables used for customer class
+    int quantity; 
+    double price; 
 
-    while(getline(transactions_file, line_transactions)) {
-        stringstream ss(line_transactions);
+    while(getline(infile, line_customers)) { //read from txt files and store into data structures
+        stringstream ss(line_customers);
         for (int i=0; getline(ss, word, ';'); i++) //breaking down the string; loops until string ends
             {
                 
                 switch (i) //goes through every part of the line
                 {
                 case 0:
-                    cID = stoi(word);
+                    id = word;
                     break;
                 case 1:
-                    oID = stoi(word); //stoi() function turns string into int
+                    first = word; 
+                    break;
+                case 2:
+                    last = word;
+                case 3:
+                    address = word;
+                case 4:
+                    city = word;
+                case 5:
+                    state = word;
+                case 6:
+                    zip = word;
+                } 
+            }
+            customers.push_back(Customer(id, first, last, address, city, state, zip)); //create a vector of Customer objects
+    }
+
+
+    while(getline(transactions_file, line_transactions)) {
+        stringstream ss(line_transactions);
+        for (int i=0; getline(ss, word, ';'); i++) 
+            {
+                
+                switch (i) 
+                {
+                case 0:
+                    cID = word;
+                    break;
+                case 1:
+                    oID = word; 
                     break;
                 } 
             }
-            transactions[oID] = Transaction(cID, oID);
+            transactions[oID] = Transaction(cID, oID); //creates transactions read from file
     }
 
 
@@ -50,22 +87,22 @@ int main() {
 
     while (getline(orders_file, line_orders)) {
         stringstream ss(line_orders);
-        for (int i=0; getline(ss, word, ';'); i++) //breaking down the string; loops until string ends
+        for (int i=0; getline(ss, word, ';'); i++) 
             {
                 
-                switch (i) //goes through every part of the line
+                switch (i) 
                 {
                 case 0:
-                    oID = stoi(word);
+                    oID = word; 
                     break;
                 case 1:
-                    date = word; //stoi() function turns string into int
+                    date = word; 
                     break;
                 case 2:
-                    quantity = stoi(word);
+                    quantity = stoi(word); //stoi() function turns string into int
                     break;
                 case 3:
-                    price = stod(word);
+                    price = stod(word); //stod() function turns string into double
                     break;
                 
                 } 
@@ -73,6 +110,12 @@ int main() {
             }
             orders.push_back(Order(oID, cID, quantity, price));
     }
+
+    while(getline(rainbow_file, line_rainbow)) {
+            id = line_rainbow;
+            rainbows.push_back(Rainbow(id));
+        }    
+        
 
 
 
@@ -86,18 +129,15 @@ int main() {
         switch(option)
         {
             case '1':
-                //customer.input(); //the function that inputs customer info, can be renamed
-                //customer.writeToFile(outfile); //the function that writes the info to the customer.txt, can be renamed
-                //outfile.flush();
-            	string first, last, id, address, city, state, zip;
+                cout << "Enter the ID of the customer: ";
+                cin >> id;
                 cout << "Enter the first name of the customer: ";
                 cin >> first;
                 cout << "Enter the last name of the customer: ";
                 cin >> last;
-                cout << "Enter the ID of the customer: ";
-                cin >> id;
                 cout << "Enter the address of the customer: ";
-                cin >> address;
+                cin.ignore();
+                getline(cin, address);
                 cout << "Enter the city the customer lives in: ";
                 cin >> city;
                 cout << "Enter the state the customer lives in: ";
@@ -105,30 +145,21 @@ int main() {
                 cout << "Enter the ZIP code of the customer: ";
                 cin >> zip;
 
-                new Customer(id, first, last, address, city, state, zip);
-                //Need to add customer to whatever we're storing them in
-                cout << first << " " << last << " has been added.";
+                customers.push_back(Customer(id, first, last, address, city, state, zip)); //adds a new Customer object to a vector
+                cout << first << " " << last << " has been added.\n";
                 break;
             case '2':
                 cout << "Enter the last name: ";
                 cin >> last_name;
-                while (getline(infile, line)) {
-                    if (line.find(last_name) != string::npos) {
-                        cout << line << endl;
-
+                for (int i = 0; i<customers.size(); i++) {
+                    if (last_name == customers[i].getLastName()) { //looks for a last name
+                        cout << customers[i].getID() << ";" << customers[i].getFirstName() << ";" << customers[i].getLastName() << ";"
+                         << customers[i].getAddress() << ";" << customers[i].getCity() << ";" << customers[i].getState() << ";" 
+                         << customers[i].getZIP() << endl; //output all instances with a last name
                     }
                 }
                 break;
-            case '2':
-                cout << "Enter the last name: ";
-                cin >> last_name;
-                while (getline(infile, line)) {
-                    if (line.find(last_name) != string::npos) {
-                        cout << line << endl;
-
-                    }
-                }
-                break;
+            
             case '3':
                 cout << "Please enter your customer ID and a quantity of tribbles you want to buy:\n";
                 cout << "1 tribble - $9.50;\n2 tribbles - $16.15;\n3 tribbles - $25.88;\n4 tribbles - $28.15;\n5 tribbles - $30.00.\n";
@@ -155,8 +186,8 @@ int main() {
                     break;
                 }
                 cout << cID << " | " << quantity << " tribble(s) |" << " $" << price << "\n";
-                orders.push_back(Order(oID, cID, quantity, price));
-                transactions[oID] = Transaction(cID, oID);
+                orders.push_back(Order(oID, cID, quantity, price)); //adds a new Order object to a vector
+                transactions[oID] = Transaction(cID, oID); //add a bew Transaction object to a map
                 break;
             case '4':
                 do {
@@ -165,19 +196,34 @@ int main() {
                 cin >> sub_option;
                 switch(sub_option) {
                     case '1':
-                        cout << "Please enter your customer ID and a quantity of tribbles you want to buy:\n";
+                        cout << "Please enter the customer ID:\n";
+                        cin >> id;
+                        rainbows.push_back(Rainbow(id)); //adds a new Rainbow object to a vector
+                        break;
                         
                     case '2':
-                        //rainbow.remove();
+                    cout << "Please enter your customer ID: ";
+                        cin >> id;
+                        for (int i=0; i<rainbows.size(); i++) {
+                            if (rainbows[i].getID() == id) { //gets rid of a Rainbow object based on the inputted ID
+                                rainbows.erase(rainbows.begin() + i);
+                                break;
+                            }
+                            
+                        }
+                        break;
+                        
+                        
+                        
                         
                     default:
-                        cout << "Please, enter one of the valid options:\n";
+                        cout << "Please, enter one of the valid options:\n"; //validation
                 } } while (sub_option != '1' && sub_option != '2');
                 break;
             case '5':
                 return 0;
             default:
-                cout << "Please, enter one of the valid options:\n";
+                cout << "Please, enter one of the valid options:\n"; //validation
         } } while (true);
     return 0;
 }
