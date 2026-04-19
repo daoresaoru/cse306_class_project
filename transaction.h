3c37b33dd4163a283c2c732499d9f4192289b3b6
daoresaoru <149461277+daoresaoru@users.noreply.github.com>
2026-04-19 17:05:50 -0500

    Phase 3 transaction.h

--- transaction.h
+++ transaction.h
@@ -6,20 +6,26 @@ using namespace std;
 class Transaction {
     private:
         string customerID, orderID;
+        int employeeID;
     public:
         Transaction() {
             customerID = "";
             orderID = "";
+            employeeID = 0;
         }
-        Transaction(string cID, string oID) {
+        Transaction(string cID, string oID, int eID) {
             customerID = cID;
             orderID = oID;
+            employeeID = eID;
         }
         string getcustomerID() {
             return customerID;
         }
         string getorderID(){
             return orderID;
+        }
+        int getemployeeID() {
+            return employeeID;
         }
 };
 #endif
